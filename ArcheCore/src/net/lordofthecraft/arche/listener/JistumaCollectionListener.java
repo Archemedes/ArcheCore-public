3bbe07a3555bb44fccd193695ff49d2fecd1bbbf
Kalen <501warhead@gmail.com>
2015-12-17 14:50:49 -0500

    Updating jistuma's events

--- ArcheCore/src/net/lordofthecraft/arche/listener/JistumaCollectionListener.java
+++ ArcheCore/src/net/lordofthecraft/arche/listener/JistumaCollectionListener.java
@@ -2,12 +2,17 @@ package net.lordofthecraft.arche.listener;
 
 import java.util.List;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Horse;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffectType;
+import org.spigotmc.event.entity.EntityMountEvent;
 
 public class JistumaCollectionListener implements Listener{
 
@@ -20,6 +25,24 @@ public class JistumaCollectionListener implements Listener{
 			if (lore==null && !p.hasPermission("potions.mayuse")){
 				e.setCancelled(true);
 				p.setItemInHand(new ItemStack(Material.GLASS_BOTTLE,1));
+			}
+		}
+		if (s.hasItemMeta() && p.isInsideVehicle()){
+			String name = s.getItemMeta().getDisplayName();
+			if (name.equalsIgnoreCase("Stoneskin Potion") && p.getVehicle().getType() == EntityType.HORSE){
+				p.leaveVehicle();
+				p.sendMessage(ChatColor.RED + "The potion you drank makes your movements sluggish, you fall off your horse");
+			}
+		}
+	}
+	
+	@EventHandler
+	public void PlayerMountingHorse(EntityMountEvent e){
+		if (e.getEntity() instanceof Player && e.getMount() instanceof Horse){
+			Player p = (Player) e.getEntity();
+			if (p.hasPotionEffect(PotionEffectType.SLOW)){
+				e.setCancelled(true);
+				p.sendMessage(ChatColor.RED + "You are too weak to mount the Horse. (You can't use Horses while having Slowness)");
 			}
 		}
 	}
