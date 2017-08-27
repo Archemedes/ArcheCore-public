0f3389ba6ac208719b6d56af8c645a301366b08d
Sporadic <pbakker@nikhef.nl>
2017-08-27 12:38:55 +0200

    Updated CommandUtil so there won't be BLOCKING HTTP REQUESTS.
    Main thread requests = very bad. Due to new UUID system. Why it wasnt
    fixed yet is beyond me.

--- src/main/java/net/lordofthecraft/arche/util/CommandUtil.java
+++ src/main/java/net/lordofthecraft/arche/util/CommandUtil.java
@@ -1,13 +1,14 @@
 package net.lordofthecraft.arche.util;
 
-import net.lordofthecraft.arche.ArcheCore;
-import net.lordofthecraft.arche.interfaces.Persona;
-import net.lordofthecraft.arche.interfaces.PersonaHandler;
 import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
 
-@SuppressWarnings("deprecation")
+import net.lordofthecraft.arche.ArcheCore;
+import net.lordofthecraft.arche.interfaces.Persona;
+import net.lordofthecraft.arche.interfaces.PersonaHandler;
+
 public class CommandUtil {
 
 	
@@ -24,19 +25,28 @@ public class CommandUtil {
 			player = a;
 		} else return null;
 		
-		OfflinePlayer p = Bukkit.getPlayer(player);
-		if(p == null) p = Bukkit.getOfflinePlayer(player);
-		if(p == null) return null;
-		
 		PersonaHandler hand = ArcheCore.getControls().getPersonaHandler();
-		return id < 0 || id > 3? hand.getPersona(p) : hand.getAllPersonas(p)[id];
+		
+		Player p = Bukkit.getPlayer(player);
+		if(p == null) return getPreloadedPersonaFromName(player);
+		else return id < 0 || id > 3? hand.getPersona(p) : hand.getAllPersonas(p)[id];
 	}
 	
 	public static Persona currentPersonaFromArg(String a){
-		OfflinePlayer p = Bukkit.getPlayer(a);
-		if(p == null) p = Bukkit.getOfflinePlayer(a);
-		if(p == null) return null;
-		
-		return ArcheCore.getControls().getPersonaHandler().getPersona(p);
+		Player p = Bukkit.getPlayer(a);
+		if(p == null) return getPreloadedPersonaFromName(a);
+		else return ArcheCore.getControls().getPersonaHandler().getPersona(p);
+	}
+	
+	private static Persona getPreloadedPersonaFromName(String name) {
+		PersonaHandler hand = ArcheCore.getControls().getPersonaHandler();
+		for(Persona[] personas : hand.getPersonas()) {
+			for(Persona persona : personas) {
+				if(persona == null) continue;
+				else if(persona.getPlayerName().equals(name)) return persona;
+				else break;
+			}
+		}
+		return null;
 	}
 }
