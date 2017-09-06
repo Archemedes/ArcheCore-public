045e1c182accc4a89e9bef23d8014368a53415da
BuildTools <unconfigured@null.spigotmc.org>
2017-09-06 17:48:48 -0400

    Fixing the Attribute tab completion

--- src/main/java/net/lordofthecraft/arche/commands/tab/CommandAttributeTabCompleter.java
+++ src/main/java/net/lordofthecraft/arche/commands/tab/CommandAttributeTabCompleter.java
@@ -18,13 +18,11 @@ public class CommandAttributeTabCompleter implements TabCompleter {
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command cmd, String command, String[] args) {
-        if (args.length >= 2 && cmd.getName().equals("attribute")) {
+        if (args.length == 2 && cmd.getName().equals("attribute")) {
             List<String> attrs = Lists.newArrayList();
             List<Attribute> attlist = Lists.newArrayList(Attribute.values());
-            if (args.length == 3) {
-                String partial = args[2];
-                attlist.removeIf(a -> !a.name().startsWith(partial));
-            }
+            String partial = args[1];
+            attlist.removeIf(a -> !a.name().startsWith(partial));
             attlist.forEach(at -> attrs.add(at.name()));
             return attrs;
         }
