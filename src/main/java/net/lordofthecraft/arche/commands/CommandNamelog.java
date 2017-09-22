232c722644fb19d9379dec4bb9246bf5f3368d64
Kalen <501warhead@gmail.com>
2017-09-21 21:33:07 -0400

    Some small fixes...

--- src/main/java/net/lordofthecraft/arche/commands/CommandNamelog.java
+++ src/main/java/net/lordofthecraft/arche/commands/CommandNamelog.java
@@ -1,7 +1,7 @@
 package net.lordofthecraft.arche.commands;
 
 import net.lordofthecraft.arche.SQL.WhySQLHandler;
-import net.lordofthecraft.arche.save.SaveHandler;
+import net.lordofthecraft.arche.save.ArcheExecutor;
 import net.lordofthecraft.arche.save.tasks.ArcheTask;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -30,7 +30,7 @@ public class CommandNamelog implements CommandExecutor {
 			} else {
 				final UUID uuid = play.getUniqueId();
 				sender.sendMessage(ChatColor.LIGHT_PURPLE + "Retrieving a Persona name history for " + ChatColor.WHITE + play.getName());
-                SaveHandler.getInstance().put(new ArcheTask() {
+                ArcheExecutor.getInstance().put(new ArcheTask() {
                     @Override
                     public void run(){
 						try{
