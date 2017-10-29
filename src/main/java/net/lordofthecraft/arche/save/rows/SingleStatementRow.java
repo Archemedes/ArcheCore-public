9c06295166fce4fed1fc9d5f4bfd1563e026348f
Sporadic <pino.bakker@gmail.com>
2017-10-29 20:39:23 +0100

    First step in rewriting rows to reduce boilerplate
    Challenge is to maintain all the features.
    
    StatementRow and SingleStatementRow meant to take over APSR and AMR
    
    AMR is redundant so consumption rewritten. This solution is also
    shorter.
    
    Batch statements retained, even in cases of multiple PrepStat per row

---
+++ src/main/java/net/lordofthecraft/arche/save/rows/SingleStatementRow.java
@@ -0,0 +1,36 @@
+package net.lordofthecraft.arche.save.rows;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.Validate;
+
+public abstract class SingleStatementRow extends StatementRow {
+		
+	@Override
+	public String toString() { //Works in most cases else override
+		String sql = getStatement();
+		String[] bits = sql.split("\\?");
+		String[] its = bits[0].split(" ");
+		bits[0] = its[its.length - 1];
+	
+		for(int i = 1; i <= bits.length; i++){
+			bits[i-1] = bits[i-1] + getValueFor(i).toString();
+		}
+		
+		return this.getClass().getSimpleName() + '{' + StringUtils.join(bits, ',') + '}';
+	}
+	
+	@Override
+	public final String[] getStatements() {
+		return new String[] {getStatement()};
+	}
+	
+	protected abstract String getStatement();
+	
+	@Override
+	public final Object getValueFor(int stat, int index) {
+		Validate.isTrue(stat == 0);
+		return getValueFor(index);
+	}
+	
+	protected abstract Object getValueFor(int index);
+}
---
+++ src/main/java/net/lordofthecraft/arche/save/rows/StatementRow.java
@@ -0,0 +1,98 @@
+package net.lordofthecraft.arche.save.rows;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+import org.apache.commons.lang.StringUtils;
+
+import net.lordofthecraft.arche.ArcheCore;
+import net.lordofthecraft.arche.SQL.ArcheSQLiteHandler;
+import net.lordofthecraft.arche.util.SQLUtil;
+
+public abstract class StatementRow implements ArcheRow {
+	private final boolean usingSqlite;
+	
+	
+	public StatementRow(){
+		usingSqlite = ArcheCore.getSQLControls() instanceof ArcheSQLiteHandler;
+	}
+	
+	@Override
+	public String[] getInserts() { //Works in most cases else override
+		String sql[] = getStatements();
+		String finalResult[] = new String[sql.length];
+		
+		for(int h = 0; h < sql.length; h++) {
+			String[] bits = sql[h].split("\\?");
+			StringBuilder result = new StringBuilder();
+			for(int i = 1; i <= bits.length; i++){
+				result.append(bits[i-1]);
+
+				Object o = getValueFor(h,i);
+				if(o instanceof String) {
+					result.append('\'').append(SQLUtil.mysqlTextEscape((String) o)).append('\'');
+				} else {
+					result.append(o.toString());
+				}
+			}
+			
+			finalResult[h] = result.toString();
+		}
+		
+		return finalResult;
+	}
+	
+	public final PreparedStatement[] prepare(Connection connection) throws SQLException {
+		String[] sql = getStatements();
+		PreparedStatement[] result = new PreparedStatement[sql.length];
+		
+		for(int h = 0; h<sql.length;h++) {
+			result[h] = connection.prepareStatement(sql[h]);
+		}
+		
+		return result;
+	}
+	
+	public final void setValues(PreparedStatement[] statements) throws SQLException {
+		String[] sqls = getStatements();
+		
+		for(int h = 0; h < sqls.length; h++) {
+			String sql = sqls[h];
+			PreparedStatement statement = statements[h];
+			int amountOfVariables = StringUtils.countMatches(sql, "?");
+			for(int i = 1; i <= amountOfVariables; i++) {
+				Object o = getValueFor(h,i);
+				
+				if(o instanceof Number) { //Numbers in order of decreasing likelihood
+					if(o instanceof Integer) {
+						statement.setInt(i, (Integer) o);
+					} else if(o instanceof Double) {
+						statement.setDouble(i, (Double) o);
+					} else if(o instanceof Long) {
+						statement.setLong(i, (Long) o);
+					} else if(o instanceof Float) {
+						statement.setFloat(i, (Float) o);
+					} else if(o instanceof Short) {
+						statement.setShort(i, (Short) o);
+					} else if (o instanceof Byte) {
+						statement.setByte(i, (Byte) o);
+					} else {
+						throw new IllegalArgumentException("Custom Number implementation!");
+					}
+				} else if(o instanceof Timestamp) {
+					statement.setTimestamp(i, (Timestamp) o);
+				} else { //String, enum, etc
+					statement.setString(i, o.toString());
+				}
+			}
+		}
+	}
+	
+	public final boolean isUsingSqlite() { return usingSqlite; }
+	
+	protected abstract String[] getStatements();
+	
+	protected abstract Object getValueFor(int statement, int varIndex);
+}
