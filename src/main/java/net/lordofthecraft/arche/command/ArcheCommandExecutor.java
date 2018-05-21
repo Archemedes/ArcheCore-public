3739bf3975149645084ff598b50bf1b77715cdd1
Sporadic <pino.bakker@gmail.com>
2018-05-21 17:26:22 +0200

    Automatically preloads Personas to be acted upon

--- src/main/java/net/lordofthecraft/arche/command/ArcheCommandExecutor.java
+++ src/main/java/net/lordofthecraft/arche/command/ArcheCommandExecutor.java
@@ -2,17 +2,22 @@ package net.lordofthecraft.arche.command;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-import com.google.common.collect.Lists;
-
 import lombok.RequiredArgsConstructor;
-import net.lordofthecraft.arche.interfaces.Persona;
+import net.lordofthecraft.arche.ArcheCore;
+import net.lordofthecraft.arche.CoreLog;
+import net.lordofthecraft.arche.interfaces.OfflinePersona;
+import net.lordofthecraft.arche.persona.ArchePersona;
+import net.lordofthecraft.arche.persona.ArchePersonaHandler;
+import net.lordofthecraft.arche.util.AsyncRunner;
+import net.lordofthecraft.arche.util.CommandUtil;
+import net.lordofthecraft.arche.util.MessageUtil;
 
 @RequiredArgsConstructor
 public class ArcheCommandExecutor implements CommandExecutor {
@@ -39,14 +44,53 @@ public class ArcheCommandExecutor implements CommandExecutor {
 		} else if (wantsHelpFlag(args)) {
 			help(sender, command);
 		} else {
-			RanCommand c = new RanCommand(command, sender);
-			c.parseAll(args);
-			
-			if(c.isInErrorState()) {
-				//TODO
+			if(command.requiresPersona() && sender.hasPermission("archecore.mod.persona")) {
+				OfflinePersona pers = personaFlag(args);
+				if(pers != null) {
+					getPersonaAndExecute(command, sender, args, pers);
+					return;
+				}
 			}
+
+			executeCommand(sender, command, args);
 		}
+	}
+	
+	private void executeCommand(CommandSender sender, ArcheCommand command, List<String> args) {
 		
+		RanCommand c = new RanCommand(command, sender);
+		c.parseAll(args);
+		
+		if(c.isInErrorState()) {
+			//TODO
+		}
+	}
+	
+	private OfflinePersona personaFlag(List<String> args) {
+		return IntStream.range(0, args.size())
+		.filter(i-> args.get(i).equals("-p"))
+		.map(i->i+1)
+		.filter(i -> i < args.size())
+		.mapToObj(args::get)
+		.findFirst()
+		.map(CommandUtil::offlinePersonaFromArg)
+		.filter(o->!o.isLoaded())
+		.orElse(null);
+	}
+	
+	private void getPersonaAndExecute(ArcheCommand ac, CommandSender sender, List<String> args, OfflinePersona o) {
+		sender.sendMessage(ChatColor.LIGHT_PURPLE + "Persona not loaded. Will try to load it now. Please wait...");
+		AsyncRunner.doAsync(ArcheCore.getPlugin(), ()->o.loadPersona())
+		.andThen(persona->{
+			ArchePersona otherPersona = ArchePersonaHandler.getInstance()
+					.getPersonaStore()
+					.addOnlinePersona((ArchePersona)persona);
+      if(otherPersona != persona) {
+      	CoreLog.warning("Interleaved Persona loading: Persona " + MessageUtil.identifyPersona(persona)
+      	+ " has come online while " + sender.getName() +  " also tried to load it.");
+      }
+      executeCommand(sender, ac, args);
+		});
 	}
 	
 	private boolean wantsHelpFlag(List<String> args) {
