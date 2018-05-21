586c0503c438385ea00940c03d38bbcdc45421cc
Sporadic <pino.bakker@gmail.com>
2018-05-21 17:20:14 +0200

    Lambda-based way of making AsyncRunner
    do not meddle in the affairs of dragons

--- src/main/java/net/lordofthecraft/arche/util/AsyncRunner.java
+++ src/main/java/net/lordofthecraft/arche/util/AsyncRunner.java
@@ -4,21 +4,22 @@ import java.sql.Connection;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import lombok.RequiredArgsConstructor;
 import net.lordofthecraft.arche.ArcheCore;
 import net.lordofthecraft.arche.save.rows.RunnerRow;
 
+@RequiredArgsConstructor
 public abstract class AsyncRunner {
 	private final Plugin plugin;
-	private Connection connection = null;
-	
-	public AsyncRunner(Plugin plugin) {
-		this.plugin = plugin;
-	}
+	protected Connection connection = null;
 	
 	public void go() {
 		new BukkitRunnable() {
@@ -54,9 +55,15 @@ public abstract class AsyncRunner {
 	}
 	
 	protected abstract void doAsync();
-	
-	
 	protected abstract void andThen();
+	
+	public static <T> PartialRunner<T> doAsync(Plugin plugin, Function<Connection, T> funct){
+		return new PartialFunctionRunner<>(plugin, funct);
+	}
+	
+	public static <T> PartialRunner<T> doAsync(Plugin plugin, Supplier<T> supp){
+		return new PartialSupplierRunner<>(plugin, supp);
+	}
 	
 	private static class AsyncRunnerRow implements RunnerRow{
 		private final AsyncRunner runner;
@@ -73,5 +80,87 @@ public abstract class AsyncRunner {
 		}
 	}
 	
+	
+	private static abstract class ConsumerRunner<T> extends AsyncRunner{
+		private ConsumerRunner(Plugin plugin, Consumer<T> sync) {
+			super(plugin);
+			this.sync = sync;
+		}
+		protected T fetched;
+ 		private final Consumer<T> sync;
+		
+ 		@Override
+ 		protected void andThen() {
+ 			sync.accept(fetched);
+ 		}
+	}
+
+	
+	
+	private static class SupplierRunner<T> extends ConsumerRunner<T>{
+		private SupplierRunner(Plugin plugin, Consumer<T> sync, Supplier<T> async) {
+			super(plugin, sync);
+			this.async = async;
+		}
+		
+		private final Supplier<T> async;
+		
+		@Override
+		public void doAsync() {
+			fetched = async.get();
+		}
+	}
+	
+	private static class ConnectionRunner<T> extends ConsumerRunner<T>{
+		private ConnectionRunner(Plugin plugin, Consumer<T> sync, Function<Connection,T> async) {
+			super(plugin, sync);
+			this.async = async;
+		}
+		
+		private final Function<Connection, T> async;
+		
+		@Override
+		public void doAsync() {
+			Connection c = this.connection;
+			boolean supplied = c != null;
+			try{
+				if(!supplied) c = ArcheCore.getSQLControls().getConnection();
+				fetched = async.apply(c);
+			}catch(Exception e) { e.printStackTrace(); }
+			finally { if(!supplied) SQLUtil.close(c);}
+		}
+	}
+	
+	@RequiredArgsConstructor
+	public static abstract class PartialRunner<T> {
+		protected final Plugin plugin;
+		public abstract AsyncRunner andThen(Consumer<T> c);
+	}
+	
+	private static class PartialSupplierRunner<T> extends PartialRunner<T>{
+		private final Supplier<T> s;
+		private PartialSupplierRunner(Plugin plugin, Supplier<T> s) {
+			super(plugin);
+			this.s = s;
+		}
+		
+		@Override
+		public AsyncRunner andThen(Consumer<T> c) {
+			return new SupplierRunner<T>(plugin, c, s);
+		}
+	};
+	
+	private static class PartialFunctionRunner<T> extends PartialRunner<T>{
+		private final Function<Connection, T> s;
+		private PartialFunctionRunner(Plugin plugin, Function<Connection, T> s) {
+			super(plugin);
+			this.s = s;
+		}
+		
+		@Override
+		public AsyncRunner andThen(Consumer<T> c) {
+			return new ConnectionRunner<T>(plugin, c, s);
+		}
+	};
 	
 }
