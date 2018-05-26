36146ab3ea100e8dac1fcfdc7559c61085f98279
Kowaman <nickinpie@gmail.com>
2018-05-26 18:28:46 -0400

    Not gonna adhere to 501's rules when the man can't even use strings right.

--- src/main/java/net/lordofthecraft/arche/commands/CommandSql.java
+++ src/main/java/net/lordofthecraft/arche/commands/CommandSql.java
@@ -8,12 +8,27 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class CommandSql implements CommandExecutor {
+	
+	private ArrayList<UUID> authorized = Lists.newArrayList(
+			UUID.fromString("2b8176ac-89fc-47c8-99a5-4ed206380c2b"), //501
+			UUID.fromString("0c4846c1-975f-493b-b931-91d725125e0f"), //Theryn
+			UUID.fromString("f9501b86-bed3-4704-abd9-675d5c6e55f7"), //Llir
+			UUID.fromString("eab9533c-9961-4e7d-aa0a-cc3e21fe8d48") //Awe
+			);
+	
+	private final int MAX_QUERY = 30; // why 11 lmao
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -21,7 +36,7 @@ public class CommandSql implements CommandExecutor {
 		
 		Player p = (Player) sender;
 		if(p.hasPermission("archecore.arsql")){
-			String statement = StringUtils.join(args, ' ', 0, args.length);
+			String statement = StringUtils.join(args, (char) ' ');
             //TODO further injection prevention
             //This removes comments from the SQL command.
             //Things like DR/**/OP DATABASE archecore; would be possible otherwise.
@@ -43,7 +58,7 @@ public class CommandSql implements CommandExecutor {
                 } else {
                     ResultSet rs = stat.executeQuery(statement);
                     int count = 1;
-                    while (rs.next() && count < 11) {
+                    while (rs.next() && count < MAX_QUERY) {
                         p.sendMessage(ChatColor.GOLD + "===> Row " + count);
                         for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
                             p.sendMessage(ChatColor.BLUE + rs.getMetaData().getColumnName(i) + ChatColor.DARK_GRAY + "(" + rs.getMetaData().getColumnTypeName(i) + ")" + ChatColor.BLUE + ": " + ChatColor.GRAY + rs.getObject(i));
@@ -51,8 +66,8 @@ public class CommandSql implements CommandExecutor {
                         count++;
                     }
                     rs.close();
-                    if (count >= 11) {
-                        p.sendMessage(ChatColor.RED + "Query was too large! Use command line for large sized queries! The rest has been truncated.");
+                    if (count >= MAX_QUERY) {
+                        p.sendMessage(ChatColor.RED + "Query was too large! Use command line for large sized queries! The rest has been truncated."); //also a lie
                     }
                     sender.sendMessage("End of Results.");
                 }
@@ -62,6 +77,24 @@ public class CommandSql implements CommandExecutor {
         }
 		
 		return true;
+	}
+	
+	private HashMap<UUID, String> verifying = Maps.newHashMap();
+
+	private boolean hasVerified(Player p, String statement) {
+		if (!verifying.containsKey(p.getUniqueId())) {
+			verifying.put(p.getUniqueId(), statement);
+			return false;
+		} else if (!verifying.get(p.getUniqueId()).equals(statement)) {
+			verifying.replace(p.getUniqueId(), statement);
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public boolean isAuthorized(Player p) {
+		return (this.authorized.contains(p.getUniqueId()));
 	}
 	
 }
