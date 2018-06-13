7740bcd454c9b7562b42251c5553a96fec97ead2
Sporadic <pino.bakker@gmail.com>
2018-06-13 22:12:48 +0200

    last() but not first()

--- src/main/java/net/lordofthecraft/arche/util/extension/UtilExtension.java
+++ src/main/java/net/lordofthecraft/arche/util/extension/UtilExtension.java
@@ -1,10 +1,12 @@
 package net.lordofthecraft.arche.util.extension;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
+import org.apache.commons.lang.ObjectUtils;
 import org.apache.commons.lang.StringUtils;
 
 public class UtilExtension {
@@ -19,7 +21,7 @@ public class UtilExtension {
 		return Optional.ofNullable(object);
 	}
  	
-	public static <T> void ifElse(Optional<T> optional, Consumer<T> theIf, Runnable theElse){
+	public static <T, E extends T> void ifElse(Optional<E> optional, Consumer<E> theIf, Runnable theElse){
 		if(optional.isPresent()) {
 			theIf.accept(optional.get());
 		} else {
@@ -31,12 +33,24 @@ public class UtilExtension {
 		return Stream.of(array);
 	}
 	
+	public static boolean eq(Object o1, Object o2) {
+		return ObjectUtils.equals(o1, o2);
+	}
+	
 	public static String join(String separator, Collection<?> parts) {
 		return join(separator, parts.toArray());
 	}
 	
 	public static String join(String separator, Object... parts) {
 		return StringUtils.join(parts, separator);
+	}
+	
+	public static <T> T last(List<T> list) {
+		return list.get(list.size() - 1);
+	}
+	
+	public static <T> T last(T[] array) {
+		return array[array.length - 1];
 	}
 	
 }
