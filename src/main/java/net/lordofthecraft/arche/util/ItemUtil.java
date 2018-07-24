0735589bd7ba5571b10b10501bbd41c69817f30f
Kowaman <nickinpie@gmail.com>
2018-07-24 22:12:11 +0000

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit 3cdbbb2e6e497d1631158b55b1586ea3b3ae4dbf

--- src/main/java/net/lordofthecraft/arche/util/ItemUtil.java
+++ src/main/java/net/lordofthecraft/arche/util/ItemUtil.java
@@ -28,8 +28,6 @@ import net.md_5.bungee.api.chat.TranslatableComponent;
 public class ItemUtil {
 	private ItemUtil() {}
 	
-	private final static UUID SKULL_ID= UUID.fromString("4fd01eb9-b99f-417c-bde4-3cb21f1a88ad");
-	
 	/**
 	 * Method to easily make Minecraft skulls from arbitrary skin files
 	 * @param theTexture A base-64 encoded nbt compound containing skin info
@@ -41,8 +39,12 @@ public class ItemUtil {
 		NbtCompound properties = NbtFactory.ofCompound("Properties");
 		NbtCompound property = NbtFactory.ofCompound("");
 		
+		char[] uid = UUID.nameUUIDFromBytes(theTexture.getBytes()).toString().toCharArray();
+		uid[14] = '4';
+		uid[19] = 'a';
+		
 		property.put("Value", theTexture);
-		skullOwner.put("Id", SKULL_ID.toString());
+		skullOwner.put("Id", String.valueOf(uid));
 		NbtList<NbtCompound> list = NbtFactory.ofList("textures", property);
 		properties.put(list);
 		skullOwner.put(properties);
