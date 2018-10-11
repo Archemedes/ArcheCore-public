65328a39f4025a52647c4ad1d28faab7615aea9d
Sporadic <pjcbakker1@gmail.com>
2018-10-11 20:27:11 +0200

    path

--- src/main/java/net/lordofthecraft/arche/util/ReflectionUtil.java
+++ src/main/java/net/lordofthecraft/arche/util/ReflectionUtil.java
@@ -3,15 +3,11 @@ package net.lordofthecraft.arche.util;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
-import org.bukkit.Bukkit;
-
 import com.comphenix.protocol.utility.MinecraftReflection;
 
 public class ReflectionUtil {
 	private ReflectionUtil() {}
 	
-	final public static String PATH = "net.minecraft.server." + Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3] + ".";
-
 	//NBT Compound tag construction
 	private static Class<?> NBTTagCompound;
 	private static Constructor<?> compoundConstructor;
@@ -22,7 +18,7 @@ public class ReflectionUtil {
 
 	static{
 		try{
-			NBTTagCompound = Class.forName(PATH + "NBTTagCompound");
+			NBTTagCompound = MinecraftReflection.getMinecraftClass("NBTTagCompound");
 			compoundConstructor = NBTTagCompound.getConstructor();
 			
 			saveToJson = MinecraftReflection.getItemStackClass().getMethod("save", NBTTagCompound);
