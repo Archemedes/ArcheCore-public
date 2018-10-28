e1a312d2173ee1120872e078fbbd99b2a619e7d7
Sporadic <pjcbakker1@gmail.com>
2018-10-28 21:49:34 +0100

    interface handles: account

--- src/main/java/net/lordofthecraft/arche/interfaces/Account.java
+++ src/main/java/net/lordofthecraft/arche/interfaces/Account.java
@@ -1,8 +1,15 @@
 package net.lordofthecraft.arche.interfaces;
 
+import java.util.List;
+
 /**
  * a simple account which can have multiple Minecraft characters
  */
 public interface Account {
 
+	List<Toon> getToons();
+	
+	Tags<Account> getTags();
+	
+	int getId();
 }
