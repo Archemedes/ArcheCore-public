d3e262a598882f6d7b4b6f05654f3796d75d0c82
Sporadic <pjcbakker1@gmail.com>
2018-12-14 18:06:38 +0100

    delivery!

--- src/main/java/net/lordofthecraft/arche/account/AccountBlob.java
+++ src/main/java/net/lordofthecraft/arche/account/AccountBlob.java
@@ -2,9 +2,10 @@ package net.lordofthecraft.arche.account;
 
 import java.util.List;
 
+import lombok.Getter;
 import net.lordofthecraft.arche.persona.ArchePersona;
 
 public class AccountBlob {
-		private ArcheAccount account;
-		private List<ArchePersona> personas;
+		@Getter private ArcheAccount account;
+		@Getter private List<ArchePersona> personas;
 }
--- src/main/java/net/lordofthecraft/arche/account/Loader.java
+++ src/main/java/net/lordofthecraft/arche/account/Loader.java
@@ -5,9 +5,10 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
-import java.util.concurrent.Future;
 
+import lombok.var;
 import net.lordofthecraft.arche.interfaces.Account;
+import net.lordofthecraft.arche.interfaces.OfflinePersona;
 import net.lordofthecraft.arche.interfaces.Persona;
 
 public class Loader {
@@ -16,12 +17,30 @@ public class Loader {
 	private final List<Waiter<Account>> accounts = new ArrayList<>();
 	private final List<Waiter<Persona>> personas = new ArrayList<>();
 
-	public synchronized void check(UUID u) {
+	public synchronized Waiter<Account> check(UUID u) {
 		
+		return null;
 	}
 	
-	public synchronized void deliver() {
+	public synchronized Waiter<Persona> check(OfflinePersona pers){
 		
+		return null;
+	}
+	
+	public synchronized void deliver(AccountBlob blob) {
+		for(var acc : accounts) {
+			blob.getAccount().getUUIDs().stream()
+				.filter(acc::isUUID)
+				.findAny().ifPresent($->acc.fulfil(blob.getAccount()));
+		}
+		
+		for(var prs : personas) {
+			blob.getPersonas().stream()
+				.filter(p->prs.isId(p.getPersonaId()))
+				.findAny().ifPresent(p->prs.fulfil(p));
+		}
+		
+		blob.getAccount().getUUIDs().forEach(u -> confirmed.add(u));
 	}
 	
 }
