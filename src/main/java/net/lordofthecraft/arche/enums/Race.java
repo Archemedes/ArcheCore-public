206c43f6125e3e7db7b0371c8b96a3a8dd7f9071
Sporadic <pjcbakker1@gmail.com>
2018-12-15 21:28:49 +0100

    CORE LORE RACE PURGE :D

--- src/main/java/net/lordofthecraft/arche/enums/Race.java
+++ src/main/java/net/lordofthecraft/arche/enums/Race.java
@@ -5,18 +5,12 @@ public enum Race {
     ELF("Elf", "Elf", 1000),
     DWARF("Dwarf", "Dwarf"),
     ORC("Orc", "Orc"),
-    KHARAJYR("Kharajyr", "Kharajyr"),
 
-    NORTHENER("Highlander", "Human", 100),
-    SOUTHERON("Farfolk", "Human", 100),
-    HEARTLANDER("Heartlander", "Human", 100),
-    ADUNIAN("Adunian", "Human", 200),
     HALFLING("Halfling", "Human", 200),
 
     HIGH_ELF("High Elf", "Elf", 1000),
     DARK_ELF("Dark Elf", "Elf", 1000),
     WOOD_ELF("Wood Elf", "Elf", 1000),
-    SNOW_ELF("Snow Elf", "Elf", 1000), //Kill me please -501warhead
 
     CAVE_DWARF("Cave Dwarf", "Dwarf"),
     FOREST_DWARF("Forest Dwarf", "Dwarf"),
@@ -25,20 +19,6 @@ public enum Race {
 
     OLOG("Olog", "Orc", 500),
     GOBLIN("Goblin", "Orc"),
-
-    KHA_LEPARDA("Kha'Leparda", "Kharajyr"),
-    KHA_TIGRASI("Kha'Tigrasi", "Kharajyr"),
-    KHA_CHEETRAH("Kha'Cheetrah", "Kharajyr"),
-    KHA_PANTERA("Kha'Pantera", "Kharajyr"),
-
-    CONSTRUCT("Construct", "Creature", 10000),
-    SPECTRE("Spectre", "Creature", 10000),
-    NECROLYTE("Necrolyte", "Creature", 10000),
-    NEPHILIM("Azdrazi", "Creature", 10000),
-    UNDEAD("Undead", "Special", 10000),
-    ASCENDED("Aengulbound", "Special", 10000),
-    AENGUL("Aengul", "Special", 10000),
-    DAEMON("Daemon", "Special", 10000),
 
     UNSET("Unset", "Unset", 1000);
 
@@ -77,7 +57,7 @@ public enum Race {
     }
 
 	public static Race fromName(String race) {
-		for (Race r : values()) 
+		for (Race r : values())
 			if (r.getName().equalsIgnoreCase(race))
 				return r;
 		return null;
