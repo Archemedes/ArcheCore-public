9f1287a4b1abc8eb2dec3f4b5fa6de67e3839556
Sporadic <pjcbakker1@gmail.com>
2018-12-24 13:03:27 +0100

    This fucker gets Brigadier for us

--- src/main/java/net/lordofthecraft/arche/command/BrigadierProvider.java
+++ src/main/java/net/lordofthecraft/arche/command/BrigadierProvider.java
@@ -1,5 +1,7 @@
 package net.lordofthecraft.arche.command;
 
+import java.lang.reflect.Method;
+
 import com.comphenix.protocol.utility.MinecraftReflection;
 import com.mojang.brigadier.CommandDispatcher;
 
@@ -9,17 +11,43 @@ import net.lordofthecraft.arche.CoreLog;
 
 public class BrigadierProvider {
 	@Getter private boolean functional = true;
-	@Getter private final CommandDispatcher<?> brigadier;
+	@Getter private CommandDispatcher<?> brigadier;
+
+	private Method getServer;
+	private Method getMCDispatcher;
+	private Method getBrigadier;
 	
 	BrigadierProvider() {
 		try {
-			var server = MinecraftReflection.getMinecraftServerClass();
-		
+			var serverClass = MinecraftReflection.getMinecraftServerClass();
+			getServer = serverClass.getMethod("getServer");
+			getMCDispatcher = serverClass.getDeclaredMethod("getCommandDispatcher");
+			getMCDispatcher.setAccessible(true);
+			getBrigadier = reflectBrigadierGetter();
+			
+			brigadier = reflectBrigadier();
 		} catch(Exception e) {
 			CoreLog.severe("We were unable to set up the BrigadierProvider. Likely a reflection error!");
 			functional = false;
 			e.printStackTrace();
 		}
+	}
+	
+	private Method reflectBrigadierGetter() throws Exception {
+		var dispatcherClass = MinecraftReflection.getMinecraftClass("CommandDispatcher");
+		for(var xx : dispatcherClass.getDeclaredMethods()) {
+			if(xx.getParameterCount() == 0 && CommandDispatcher.class.isAssignableFrom(xx.getReturnType()))
+				return xx;
+		}
+		
+		throw new NoSuchMethodError("Fuck");
+	}
+	
+	
+	private CommandDispatcher<?> reflectBrigadier() throws Exception {
+		var server = getServer.invoke(null); //Static MinecraftServer getter
+		var dispatch = getMCDispatcher.invoke(server);
+		return (CommandDispatcher<?>) getBrigadier.invoke(dispatch);
 	}
 	
 
