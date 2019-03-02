490fe647c36e220d257dcc01b2e11973545a9c9e
Kowaman <nickinpie@gmail.com>
2019-03-02 05:39:29 -0500

    Attribute bandaid

--- src/main/java/net/lordofthecraft/arche/attributes/AttributeRegistry.java
+++ src/main/java/net/lordofthecraft/arche/attributes/AttributeRegistry.java
@@ -9,28 +9,28 @@ import org.bukkit.attribute.Attribute;
 
 public class AttributeRegistry {
 
-    public static final ArcheAttribute MAX_FATIGUE = new AttributeMaxFatigue("Maximum Fatigue", 100.0);
-    public static final ArcheAttribute FATIGUE_GAIN = new ArcheAttribute("Fatigue Gained", 1, false);
-    public static final ArcheAttribute EXHAUSTION = new ArcheAttribute("Exhaustion", 0, false);
-    public static final ArcheAttribute MOB_DAMAGE_DEAL = new ArcheAttribute("Damage to Mobs", 1.0);
-    public static final ArcheAttribute MOB_DAMAGE_TAKE = new ArcheAttribute("Damage from Mobs", 1.0, false);
-    public static final ArcheAttribute REGENERATION = new ArcheAttribute("Regeneration", 1.0);
-    public static final ArcheAttribute HUNGER = new ArcheAttribute("Hunger", 1.0, false);
-    public static final ArcheAttribute ARROW_DAMAGE = new ArcheAttribute("Archery Damage", 1.0);
-    public static final ArcheAttribute ARROW_VELOCITY = new ArcheAttribute("Arrow Velocity", 1.0);
-    public static final ArcheAttribute PERCEPTION = new ArcheAttribute("Perception", 0);
-    public static final ArcheAttribute SHROUD = new ArcheAttribute("Shroud", 0);
+    public static final ArcheAttribute MAX_FATIGUE = new AttributeMaxFatigue("maximum-fatigue", 100.0);
+    public static final ArcheAttribute FATIGUE_GAIN = new ArcheAttribute("fatigue-gained", 1, false);
+    public static final ArcheAttribute EXHAUSTION = new ArcheAttribute("exhaustion", 0, false);
+    public static final ArcheAttribute MOB_DAMAGE_DEAL = new ArcheAttribute("damage-to-mobs", 1.0);
+    public static final ArcheAttribute MOB_DAMAGE_TAKE = new ArcheAttribute("damage-from-mobs", 1.0, false);
+    public static final ArcheAttribute REGENERATION = new ArcheAttribute("regeneration", 1.0);
+    public static final ArcheAttribute HUNGER = new ArcheAttribute("hunger", 1.0, false);
+    public static final ArcheAttribute ARROW_DAMAGE = new ArcheAttribute("archery-damage", 1.0);
+    public static final ArcheAttribute ARROW_VELOCITY = new ArcheAttribute("arrow-velocity", 1.0);
+    public static final ArcheAttribute PERCEPTION = new ArcheAttribute("perception", 0);
+    public static final ArcheAttribute SHROUD = new ArcheAttribute("shroud", 0);
     
     //Resistance attributes
-    public static final ArcheAttribute POISON_RESISTANCE = new ArcheAttribute("Poison Resistance", 1.0);
-    public static final ArcheAttribute FIRE_RESISTANCE = new ArcheAttribute("Fire Resistance", 1.0);
-    public static final ArcheAttribute WITHER_RESISTANCE = new ArcheAttribute("Wither Resistance", 1.0);
-    public static final ArcheAttribute MAGIC_RESISTANCE = new ArcheAttribute("Magic Resistance", 1.0);
-    public static final ArcheAttribute DROWNING_RESISTANCE = new ArcheAttribute("Drowning Resistance", 1.0);
-    public static final ArcheAttribute BLAST_RESISTANCE = new ArcheAttribute("Blast Resistance", 1.0);
-    public static final ArcheAttribute PROJECTILE_RESISTANCE = new ArcheAttribute("Projectile Resistance", 1.0);
-    public static final ArcheAttribute LIGHTNING_RESISTANCE = new ArcheAttribute("Lightning Resistance", 1.0);
-    public static final ArcheAttribute FALL_RESISTANCE = new ArcheAttribute("Fall Resistance", 1.0);
+    public static final ArcheAttribute POISON_RESISTANCE = new ArcheAttribute("poison-resistance", 1.0);
+    public static final ArcheAttribute FIRE_RESISTANCE = new ArcheAttribute("fire-resistance", 1.0);
+    public static final ArcheAttribute WITHER_RESISTANCE = new ArcheAttribute("wither-resistance", 1.0);
+    public static final ArcheAttribute MAGIC_RESISTANCE = new ArcheAttribute("magic-resistance", 1.0);
+    public static final ArcheAttribute DROWNING_RESISTANCE = new ArcheAttribute("drowning-resistance", 1.0);
+    public static final ArcheAttribute BLAST_RESISTANCE = new ArcheAttribute("blast-resistance", 1.0);
+    public static final ArcheAttribute PROJECTILE_RESISTANCE = new ArcheAttribute("projectile-resistance", 1.0);
+    public static final ArcheAttribute LIGHTNING_RESISTANCE = new ArcheAttribute("lightning-resistance", 1.0);
+    public static final ArcheAttribute FALL_RESISTANCE = new ArcheAttribute("fall-resistance", 1.0);
 
     private static AttributeRegistry INSTANCE = new AttributeRegistry();
     public Map<String, ArcheAttribute> registeredAttributes = new HashMap<>();
