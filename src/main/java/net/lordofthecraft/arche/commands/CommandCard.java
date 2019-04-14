7ed6a120246921daff5ae90bccb70b301c29c9c0
Sporadic <pino.bakker@gmail.com>
2019-04-14 20:57:31 +0200

    make card send lol

--- src/main/java/net/lordofthecraft/arche/commands/CommandCard.java
+++ src/main/java/net/lordofthecraft/arche/commands/CommandCard.java
@@ -1,15 +1,19 @@
 package net.lordofthecraft.arche.commands;
 
+import java.util.List;
+
 import org.bukkit.command.CommandSender;
 
 import co.lotc.core.command.CommandTemplate;
 import net.lordofthecraft.arche.ArcheCore;
 import net.lordofthecraft.arche.interfaces.Persona;
+import net.md_5.bungee.api.chat.BaseComponent;
 
 public class CommandCard extends CommandTemplate {
 
 	public void invoke(CommandSender s, Persona target) {
-		ArcheCore.getPersonaControls().whois(target, s);
+		List<BaseComponent> whois = ArcheCore.getPersonaControls().whois(target, s);
+		whois.forEach(s.spigot()::sendMessage);
 	}
 
 }
