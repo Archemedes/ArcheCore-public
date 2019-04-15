9f5ce11a3c8e22f21f3b90e47964cca907f67584
George Lin <17georgel@gmail.com>
2019-04-16 03:45:37 +0800

    Add default to card

--- src/main/java/net/lordofthecraft/arche/commands/CommandCard.java
+++ src/main/java/net/lordofthecraft/arche/commands/CommandCard.java
@@ -1,17 +1,17 @@
 package net.lordofthecraft.arche.commands;
 
-import java.util.List;
-
-import org.bukkit.command.CommandSender;
-
 import co.lotc.core.command.CommandTemplate;
+import co.lotc.core.command.annotate.Default;
 import net.lordofthecraft.arche.ArcheCore;
 import net.lordofthecraft.arche.interfaces.Persona;
 import net.md_5.bungee.api.chat.BaseComponent;
+import org.bukkit.command.CommandSender;
+
+import java.util.List;
 
 public class CommandCard extends CommandTemplate {
 
-	public void invoke(CommandSender s, Persona target) {
+	public void invoke(CommandSender s, @Default("@p") Persona target) {
 		List<BaseComponent> whois = ArcheCore.getPersonaControls().whois(target, s);
 		whois.forEach(s.spigot()::sendMessage);
 	}
