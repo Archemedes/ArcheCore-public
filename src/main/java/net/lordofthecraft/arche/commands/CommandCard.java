9414b76e112c4581c1545b89326e029bbe173b93
George Lin <17georgel@gmail.com>
2019-04-16 16:14:47 +0800

    Ya this doesn't work.

--- src/main/java/net/lordofthecraft/arche/commands/CommandCard.java
+++ src/main/java/net/lordofthecraft/arche/commands/CommandCard.java
@@ -1,7 +1,6 @@
 package net.lordofthecraft.arche.commands;
 
 import co.lotc.core.command.CommandTemplate;
-import co.lotc.core.command.annotate.Default;
 import net.lordofthecraft.arche.ArcheCore;
 import net.lordofthecraft.arche.interfaces.Persona;
 import net.md_5.bungee.api.chat.BaseComponent;
@@ -11,7 +10,7 @@ import java.util.List;
 
 public class CommandCard extends CommandTemplate {
 
-	public void invoke(CommandSender s, @Default("@p") Persona target) {
+	public void invoke(CommandSender s, Persona target) {
 		List<BaseComponent> whois = ArcheCore.getPersonaControls().whois(target, s);
 		whois.forEach(s.spigot()::sendMessage);
 	}
