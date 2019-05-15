50a7ae33a5bc6d589a7840be68e0324a62409e49
Sporadic <pino.bakker@gmail.com>
2019-05-15 07:28:29 +0200

    this is for the ones who dont know how good they got it

--- src/main/java/net/lordofthecraft/arche/persona/PersonaAttributes.java
+++ src/main/java/net/lordofthecraft/arche/persona/PersonaAttributes.java
@@ -201,7 +201,7 @@ public class PersonaAttributes {
     	if(p != null) {
     		ArcheAttributeInstance aai = customAttributes.get(va);
     		AttributeInstance ai = p.getAttribute(va.getHandle());
-    		if(ai != null) aai.getModifiers().forEach(ai::removeModifier);
+    		if(aai != null && ai != null) aai.getModifiers().forEach(ai::removeModifier);
         }
     }
     
