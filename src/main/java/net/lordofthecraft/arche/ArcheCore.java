fe05e2c86940670028e34fa08fd53e4d2f708883
Sporadic <pino.bakker@gmail.com>
2019-07-11 17:40:34 +0200

    Tweak help files to reflect current state of lotc

--- src/main/java/net/lordofthecraft/arche/ArcheCore.java
+++ src/main/java/net/lordofthecraft/arche/ArcheCore.java
@@ -513,9 +513,10 @@ public class ArcheCore extends JavaPlugin implements IArcheCore {
 
 		String attributes = ChatColor.GRAY + "Attributes are the various base stats of your Persona. These stats can be anything from how fast your Persona runs to how much fatigue they accumulate. \n"
 				+ ChatColor.GREEN + "Various modifiers on items, spells and foodstuffs can affect these attributes for better or worse.\n"
-				+ ChatColor.LIGHT_PURPLE  + "Current persona modifiers are shown on an icon in the $/me$ " + ChatColor.LIGHT_PURPLE + "menu.";
+				+ ChatColor.LIGHT_PURPLE  + "Current attributes are shown on an icon in the " + ChatColor.WHITE + "$/menu$.";
 
 
+		@SuppressWarnings("unused")
 		String fatigue = ChatColor.GRAY + "Fatigue accumulates when practicing your chosen @@<Professions>profession@@.\n"
 				+ ChatColor.GREEN + "You can still do everything while fatigued, but will not have access to your profession-related perks.\n"
 				+ ChatColor.LIGHT_PURPLE  + "your fatigue will reset over time, so it's best to simply take a break and wait it out. "
@@ -524,8 +525,9 @@ public class ArcheCore extends JavaPlugin implements IArcheCore {
 		addHelp("Persona", persHelp, Material.COMPARATOR, "archecore.mayuse");
 		addHelp("Commands", commandHelp, Material.COMMAND_BLOCK);
 		//addHelp("Professions", profession, Material.BEDROCK);
-		addHelp("Attributes", attributes, Material.GOLDEN_APPLE);   
-		addHelp("Fatigue", fatigue, Material.RED_BED);
+		addHelp("Attributes", attributes, Material.GOLDEN_APPLE);
+		//addHelp("Fatigue", fatigue, Material.RED_BED);
+
 		//Create config-set Help Files
 		if(!(new File(getDataFolder(), "helpfiles.yml").exists()))
 			saveResource("helpfiles.yml", false);
